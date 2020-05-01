90fc5f747709316f992685ece79a28ad7b95130f
Stanley Huang <huang.stanley050@gmail.com>
2020-05-01 13:26:31 +1000

    user model

---
+++ user.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  email: String,
+  firstName: String,
+  lastName: String,
+  address: String,
+});
+
+export default mongoose.model("User".userSchema);
